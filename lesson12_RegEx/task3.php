49ba7a67dd9ca0ca2f34941b3e74f6100f93372e
sscoree <sscoree@gmail.com>
2023-01-10 21:35:09 +0200

    task3 updated

--- lesson12_RegEx/task3.php
+++ lesson12_RegEx/task3.php
@@ -10,3 +10,14 @@ Jeigu telefono numeris validus, iš funkcijos turėtų grįžti tvarkingai sufor
 "+370-623 45678" --> "+37062345678"
 */
 
+function task3(string $mobile): string
+{
+    $pattern = '/^(\+|0{2})(370)( |\-)(6)(\d{2})( |\-)(\d{5})$/';
+    $replacement = '+$2$4$5$7';
+    $result = preg_match($pattern, $mobile);
+    if ($result !== 0) {
+        return preg_replace($pattern, $replacement, $mobile);
+    }
+    return $mobile;
+}
+var_dump(task3('+370-623-45678'));
