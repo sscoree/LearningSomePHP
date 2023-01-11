23b346ccffb4e6b5493de1edde66aedfea2c77fc
sscoree <sscoree@gmail.com>
2023-01-11 21:35:47 +0200

    lesson13 part1 finished

--- lesson13_upload_download/upload.php
+++ lesson13_upload_download/upload.php
@@ -5,20 +5,38 @@ if ($error !== UPLOAD_ERR_OK){
     echo 'Error uploading file '. $_FILES['task_upload']['name'];
     die;
 }
-//var_dump($_FILES['task_upload']['type']);
-//die();
+
 $uploadType = $_FILES['task_upload']['type'];
-//$wantedTypes = ['jpeg', 'jpg', 'png', 'bmp'];
+$uploadSize = $_FILES['task_upload']['size'];
+$maxSize = 1000000;
+$acceptable = [
+    'image/jpeg',
+    'image/jpg',
+    'image/png',
+    'image/bmp'
+];
 
-
-if ($uploadType === 'image/jpeg' || $uploadType === 'image/jpg' || $uploadType === 'image/png' || $uploadType === 'image/bmp'){
+if (in_array($uploadType, $acceptable) && $maxSize > $uploadSize){
     $uploadedFileName = $_FILES['task_upload']['name'];
     $fileSavePath = './data/'. uniqid().'_'.$uploadedFileName;
     $tempFilePath = $_FILES['task_upload']['tmp_name'];
     move_uploaded_file($tempFilePath, $fileSavePath);
+
+    $metaDataFile = './meta/meta_data.json';
+    $metaDataJson = file_get_contents($metaDataFile);
+    $data = json_decode($metaDataJson, true);
+    $data[] = [
+        'name' => $_FILES['task_upload']['name'],
+        'size' => $_FILES['task_upload']['size'],
+        'path' => $fileSavePath,
+        'date' => date_create()->format('Y-m-d H:i')
+    ];
+    $serializedData = json_encode($data, JSON_PRETTY_PRINT);
+    file_put_contents($metaDataFile, $serializedData);
+
     echo "Your file upload was successful";
 } else {
-    echo "Upload file type error!";
+    echo "Upload file type or size error!";
     die();
 }
 
