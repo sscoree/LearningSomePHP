97346447b8b12658413454722ccdabd9ef7eec90
sscoree <sscoree@gmail.com>
2023-01-17 19:51:10 +0200

    task3 updated

---
+++ lesson14_practice/task3.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Išspausdinkite skaičius, kurie yra mažesni nei
+$number ir dalijasi iš 55. Jeigu paduotas skaičius mažesnis nei 0,
+funkcija paverčia šį skaičių pliusiniu.
+Funkcijos kvietimas: exercise10(60)
+ */
+
+function task3($number)
+{
+    $number = abs($number);
+    for ($i = $number - 1; $i >= 55; $i--){
+        if ($i % 55 === 0){
+            echo $i.PHP_EOL;
+        }
+    }
+}
+print_r(task3(551));
